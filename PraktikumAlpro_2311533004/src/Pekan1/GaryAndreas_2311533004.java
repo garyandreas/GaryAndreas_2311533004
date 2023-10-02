04dc8cf0fe7e29895a60325b622df8f54edd5694
garyandreas <garyandreas0804@gmail.com>
2023-10-03 00:10:56 +0700

    revisi tugas

---
+++ PraktikumAlpro_2311533004/src/Pekan1/GaryAndreas_2311533004.java
@@ -0,0 +1,11 @@
+package Pekan1;
+
+public class GaryAndreas_2311533004 {
+
+public static void main(String[] args) {
+	String nama="Gary Andreas";
+	String nim="2311533004";
+	
+		System.out.println(nama+"_"+nim);
+	}
+}
