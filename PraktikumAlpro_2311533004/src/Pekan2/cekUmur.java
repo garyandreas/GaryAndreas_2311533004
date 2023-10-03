e7920b4da75202b0085d2b5832a2faba53f8a571
garyandreas <garyandreas0804@gmail.com>
2023-10-03 09:10:48 +0700

    praktikum pekan2

---
+++ PraktikumAlpro_2311533004/src/Pekan2/cekUmur.java
@@ -0,0 +1,23 @@
+package Pekan2;
+
+import java.util.Scanner;
+
+public class cekUmur {
+
+	public static void main(String[] args) {
+	int umur;
+	Scanner a = new Scanner(System.in);
+	System.out.println("Input umur anda : ");
+	umur = a.nextInt();
+	a.close();
+		
+		if(umur > 17) {
+			System.out.println("Anda Sudah dewasa");
+			
+		} else {
+			System.out.println("Anda masih Anak-anak");
+		}
+	
+	}
+
+}
