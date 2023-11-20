07fd7d05863876e351c333463f281915c6c3fabc
garyandreas <garyandreas0804@gmail.com>
2023-11-20 19:53:17 +0700

    Pekan ke-5 (2)

--- PraktikumAlpro_2311533004/src/Pekan5/perulanganWhile3.java
+++ PraktikumAlpro_2311533004/src/Pekan5/perulanganWhile3.java
@@ -7,38 +7,35 @@ public class perulanganWhile3 {
 	public static void main(String[] args) {
 		int umur;
 		char sim;
-		// deklarasi variabel
-		String jawab;
+		String jawab = "ya";
 		
-		// deklarasi scanner
 		Scanner scan = new Scanner(System.in);
-		do {
-			System.out.println("Input umur anda : ");
-			umur = scan.nextInt();
-			System.out.println("Apakah Anda Sudah Punya SIM C?");
-			sim = scan.next().charAt(0);
-			if ((umur>=17)&&(sim=='y')) {
-				System.out.println("Anda Sudah dewasa dan boleh membawa motor");
-			}
-			if ((umur>=17)&&(sim!='y')) {
-				System.out.println("Anda sudah dewasa tetapi belum boleh membawa motor");
-			
-				
-			}
-			if((umur<17)&&(sim!='y')) {
-				System.out.println("Anda belum cukup umur untuk membawa motor");
-				
-			}
-			if((umur<17)&&(sim=='y')) {
-				System.out.println("Anda belum cukup umur untuk punya SIM");
-			
-			}
-				System.out.println("Apakah lanjut (ya / tidak)");
-			jawab=scan.next();
-			}
-			while(!jawab.equalsIgnoreCase("tidak"));
-			System.out.println("Terimakasih sudah menggunakan produk kami");
-			scan.close();
-	
+		while(!jawab.equalsIgnoreCase("tidak")) {
+		System.out.print("input umur anda: ");
+		umur = scan.nextInt();
+		
+		System.out.print("apakah anda sudah mempunyai sim c: ");
+		sim=scan.next().charAt(0);
+		
+		if ( (umur >= 17) && (sim=='y')) {
+			System.out.println("anda sudah dewasa dan boleh bawa motor ");
 		}
+		if ( (umur >= 17) && (sim!='y')) {
+			System.out.println("anda sudah dewasa tetapi belum boleh bawa motor ");
+		}
+		if ( (umur < 17) && (sim !='y')) {
+			System.out.println("anda belum cukup umur bawa motor ");
+		}
+		if ( (umur < 17) && (sim =='y')) {
+			System.out.println("anda belum cukup umur punya SIM ");
+		}
+	
+		System.out.println("Apakah lanjut (ya/tidak?)");
+		jawab= scan.next();
+		
+		}
+		System.out.println("terimakasih sudah menggunakan program ini");
+	    scan.close();
+	
+	}
 }
